8738411e705027e1e65aa4f12628e34b3adf3c75
Tandila69 <g_tandilashvili@cu.edu.ge>
2018-12-14 17:18:41 +0400

    add chart
    new feature

---
+++ javascript/chart.js
@@ -0,0 +1,55 @@
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		var config = {
+			type: 'line',
+			data: {
+				labels: [],
+				datasets: [{
+					label: 'მიმდინარე წონა',
+					backgroundColor: window.chartColors.red,
+					borderColor: window.chartColors.red,
+					data: [],
+					fill: false,
+				}, ]
+			},
+		
+		};
+
+		window.onload = function() {
+			var ctx = document.getElementById('canvas').getContext('2d');
+			window.myLine = new Chart(ctx, config);
+		};
+	
+
+		function datasett() {
+			daachira = 1;
+			$("#wona2").toggleClass("show");
+		}
+
+		function addWona() {
+			if($("#wona").val()<0) {
+				alert("რანაირად? "  + $("#wona").val());
+				return 0;
+			}
+			mtvleli = 1;
+			var mimdinarewona = $("#wona").val();
+			console.log("shevida" + mimdinarewona);	
+
+			var month = MONTHS[config.data.labels.length % MONTHS.length];
+				config.data.labels.push(month);
+			config.data.datasets.forEach(function(dataset) {
+				dataset.data.push(mimdinarewona);
+			});
+			window.myLine.update();
+		
+		}
+
+		document.getElementById('removeData').addEventListener('click', function() {
+			config.data.labels.splice(-1, 1); //წაშლა
+
+			config.data.datasets.forEach(function(dataset) {
+				dataset.data.pop();
+			});
+
+			window.myLine.update();
+		});
+
---
+++ js/utils.js
@@ -0,0 +1,80 @@
+'use strict';
+
+window.chartColors = {
+	red: 'rgb(255, 99, 132)',
+};
+
+(function(global) {
+	var Months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
+
+
+	var Samples = global.Samples || (global.Samples = {});
+	var Color = global.Color;
+
+	Samples.utils = {
+
+		srand: function(seed) {
+			this._seed = seed;
+		},
+
+
+		labels: function(config) {
+			var cfg = config || {};
+			var min = cfg.min || 0;
+			var max = cfg.max || 100;
+			var count = cfg.count || 8;
+			var step = (max - min) / count;
+			var decimals = cfg.decimals || 8;
+			var dfactor = Math.pow(10, decimals) || 0;
+			var prefix = cfg.prefix || '';
+			var values = [];
+			var i;
+
+			for (i = min; i < max; i += step) {
+				values.push(prefix + Math.round(dfactor * i) / dfactor);
+			}
+
+			return values;
+		},
+
+		months: function(config) {
+			var cfg = config || {};
+			var count = cfg.count || 12;
+			var section = cfg.section;
+			var values = [];
+			var i, value;
+
+			for (i = 0; i < count; ++i) {
+				value = Months[Math.ceil(i) % 12];
+				values.push(value.substring(0, section));
+			}
+
+			return values;
+		},
+
+		color: function(index) {
+			return COLORS[index % COLORS.length];
+		},
+
+		transparentize: function(color, opacity) {
+			var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+			return Color(color).alpha(alpha).rgbString();
+		}
+	};
+
+
+
+}(this));
